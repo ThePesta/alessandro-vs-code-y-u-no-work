ba00428626ff6a2d8b271f433a1ffda4ccae8f5b
Jason Cheung <jckaichu@gmail.com>
2020-05-18 22:06:15 +0100

    umm weee connected uhmmm weee (laughing sounds) - that's about it

--- src/app_main.rs
+++ src/app_main.rs
@@ -1,9 +1,12 @@
 use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer, Responder, Result};
-use serde::Deserialize;
+use serde::{Deserialize, Serialize};
+use uuid::Uuid;
+use island::Island;
 
 mod island {
-    use uuid::Uuid;
+    use serde::Deserialize;
     use std::str::FromStr;
+    use uuid::Uuid;
 
     struct Name(String);
 
@@ -17,31 +20,31 @@ mod island {
         }
     }
 
-    struct Island {
-        id: Uuid,
+    pub struct Island {
+        pub id: Uuid,
         owner_id: Uuid,
         name: Name,
         is_active: bool,
     }
 
     impl Island {
-        fn new(command: CreateIsland) -> Result<Self, String> {
+        pub fn new(command: CreateIsland) -> Result<Self, String> {
             let name = Name::new(command.name)?;
-            let owner_id = Uuid::from_str(&command.owner_id).map_err(|_| "Invalid owner id")?;
 
-            let island = Island{
+            let island = Island {
                 id: Uuid::new_v4(),
-                owner_id,
+                owner_id: command.owner_id,
                 name,
                 is_active: command.is_active,
             };
-            
+
             Ok(island)
         }
     }
 
-    struct CreateIsland {
-        owner_id: String,
+    #[derive(Debug, Deserialize)]
+    pub struct CreateIsland {
+        owner_id: Uuid,
         name: String,
         is_active: bool,
     }
@@ -50,37 +53,32 @@ mod island {
     mod island_tests {
         use super::*;
 
-        #[test]
-        fn create_island_invalid_owner_id_() {
-            let test_case = CreateIsland{
-                // owner_id: Uuid::new_v4().to_string(),
-                owner_id: "this is not a uuid".into(),
-                name: "ok".into(),
+        fn default_create_island() -> CreateIsland {
+            CreateIsland {
+                owner_id: Uuid::new_v4(),
+                name: "valid name".into(),
                 is_active: true,
-            };
-            
-            assert!(Island::new(test_case).is_err());
+            }
         }
 
         #[test]
         fn create_island_invalid_name() {
-            let test_case = CreateIsland{
-                owner_id: Uuid::new_v4().to_string(),
+            let test_case = CreateIsland {
                 name: "".into(),
-                is_active: true,
+                ..default_create_island()
             };
-            
+
             assert!(Island::new(test_case).is_err());
         }
 
         #[test]
         fn create_island_valid_command() {
-            let test_case = CreateIsland{
-                owner_id: Uuid::new_v4().to_string(),
+            let test_case = CreateIsland {
+                owner_id: Uuid::new_v4(),
                 name: "valid name".into(),
                 is_active: true,
             };
-            
+
             assert!(Island::new(test_case).is_ok());
         }
     }
@@ -88,13 +86,12 @@ mod island {
     #[cfg(test)]
     mod name_tests {
         use super::*;
-        use proptest::prelude::*;
 
         #[test]
         fn weird_name() {
             let test_case = "vy꙲ꙈᴫѱΆῨῨ";
 
-            assert!(Name::new(test_case.into()).is_ok())        
+            assert!(Name::new(test_case.into()).is_ok())
         }
 
         #[test]
@@ -113,24 +110,24 @@ mod island {
     }
 }
 
-async fn index(_req: HttpRequest) -> impl Responder {
-  println!("{:?}", _req);
+async fn post_island_request_handler(command: web::Json<island::CreateIsland>) -> impl Responder {
+    println!("{:?}", command);
 
-  HttpResponse::Ok().body("Hello world!")
-}
+    #[derive(Serialize)]
+    struct Response {
+        id: Uuid
+    }
 
-async fn index2() -> impl Responder {
-    HttpResponse::Ok().body("Hello world again!")
+    match Island::new(command.into_inner()) {
+        Ok(Island { id, .. }) => HttpResponse::Ok().json(Response{ id }),
+        Err(_) => HttpResponse::BadRequest().finish(),
+    }
 }
 
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
-        App::new()
-            .route("/", web::get().to(index))
-            .route("/again", web::get().to(index2))
-    })
-    .bind("127.0.0.1:8088")?
-    .run()
-    .await
+    HttpServer::new(|| App::new().route("/islands", web::post().to(post_island_request_handler)))
+        .bind("0.0.0.0:8088")?
+        .run()
+        .await
 }
